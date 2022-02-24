c45a157cefe9a2b6e703fa2ebec679ea4798ef9c
Juan <jlizarraga@unomaha.edu>
2022-02-24 13:03:46 -0600

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('books/', views.book_list, name='book_list'),
     path('books/<slug:genre_slug>/', views.book_list, name='books_list_by_genre'),
+    path('create/', views.BookCreateView.as_view(), name='create_book'),
     path('authors/', views.author_list, name='author_list')
 ]
