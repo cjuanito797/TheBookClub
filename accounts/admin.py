544a490f4f03b4c442554c26d90fe6bf1f92b1b9
Juan <jlizarraga@unomaha.edu>
2022-02-25 13:08:05 -0600

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .forms import RegistrationForm
 from .models import User
 
 
 class CustomUserAdmin (UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
+    add_form = RegistrationForm
+    form = RegistrationForm
     model = User
     list_display = ('email', 'first_name', 'last_name',)
     list_filter = ('email', 'is_staff', 'is_active',)
