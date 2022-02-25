5a4d1dba7cb23765c5e10e382f2333ca386482f2
Juan <jlizarraga@unomaha.edu>
2022-02-25 13:07:53 -0600

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,25 +1,21 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponseRedirect
 from .models import User
-
 from django.contrib.auth import login, authenticate
 from django.urls import reverse
 from django.views.generic import FormView, TemplateView
-from .forms import userRegistation, CustomUserCreationForm
+from .forms import RegistrationForm
 
 
 # Create your views here.
-def Signup(request):
-    context = {}
-    if request.POST:
-        form = CustomUserCreationForm(request.POST)
+class registration_view(FormView):
+    def post(self, request):
+        form = RegistrationForm(request.POST)
+
         if form.is_valid():
             form.save()
-            email = form.cleaned_data.get('email')
-            raw_password = form.cleaned_data.get('password')
-            user = authenticate(email=email, password=raw_password)
-            login(request, user)
-            return redirect('library:home')
-        else: #GET request
-            form = CustomUserCreationForm()
-            context['registration_form'] = form
-        return render(request, 'registration/register.html', context)
+            return redirect(reverse('library:home'))
+        return render(request, 'registration/register.html', {'form' : form})
+
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, 'registration/register.html', {'form' : form})
