21996c2ffd085928bfbfc5310484dd1055f75aa9
Juan <jlizarraga@unomaha.edu>
2022-02-25 13:07:54 -0600

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, re_path
 
+
 from . import views
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', views.Signup, name='Signup'),
+    path('register/', views.registration_view.as_view(), name='register'),
 ]
