ed01cf5f4b8cfcafea1480ecd9b2c97a0e5ae4b0
Juan <jlizarraga@unomaha.edu>
2022-03-03 10:10:39 -0600

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from .models import User
 from django.contrib.auth import login, authenticate
 from django.urls import reverse
 from django.views.generic import FormView, TemplateView
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -19,3 +20,26 @@ class registration_view(FormView):
     def get(self, request):
         form = RegistrationForm()
         return render(request, 'registration/register.html', {'form' : form})
+
+def user_login(request):
+    form = LoginForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+        user = authenticate(request,
+                            username=cd['username'],
+                            password=cd['password'])
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'home.html')
+            else:
+                return HttpResponse('Disabled Account')
+        else:
+            return HttpResponse('Invalid Login')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form' : form})
+
+@login_required
+def customerView(request):
+    return render(request, 'accounts/base.html')
