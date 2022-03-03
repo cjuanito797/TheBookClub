eea5ed3d6c6abafaa744bbbd59b09f299ef30992
Juan <jlizarraga@unomaha.edu>
2022-03-03 10:10:18 -0600

    Update urls.py

--- TheBookClub/urls.py
+++ TheBookClub/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path ('accounts/', include ('accounts.urls', namespace='accounts')),
-    path('', include('library.urls'))
+    path ('library/', include ('library.urls', namespace='library')),
+    path('', include('accounts.urls'))
 ]
