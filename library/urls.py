9a43a5195d5599fc86446c5af47495631877d71b
Juan <jlizarraga@unomaha.edu>
2022-03-10 16:15:05 -0600

    Update urls.py

--- library/urls.py
+++ library/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'library'
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('books/', views.book_list, name='book_list'),
     path('books/<slug:genre_slug>/', views.book_list, name='books_list_by_genre'),
     path('create/', views.BookCreateView.as_view(), name='create_book'),
