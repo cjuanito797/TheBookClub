09907a853f59832079078b4626591472ab26b9aa
Ghafar <47232790+abghrni@users.noreply.github.com>
2022-04-12 10:13:33 -0500

    Contact Us page
    Contact Us page

--- library/urls.py
+++ library/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, re_path
 
 from . import views
 
-app_name = 'library'
+app_name = "library"
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('books/', views.book_list, name='book_list'),
-    path('books/<slug:genre_slug>/', views.book_list, name='books_list_by_genre'),
-    path('create/', views.BookCreateView.as_view(), name='create_book'),
-    path('authors/', views.author_list, name='author_list')
+    path("", views.home, name="home"),
+    path("books/", views.book_list, name="book_list"),
+    path("books/<slug:genre_slug>/", views.book_list, name="books_list_by_genre"),
+    path("create/", views.BookCreateView.as_view(), name="create_book"),
+    path("authors/", views.author_list, name="author_list"),
+    path("contactus", views.contactus, name="contactus"),
 ]
--- library/views.py
+++ library/views.py
@@ -8,70 +8,65 @@ from accounts.views import customerView
 
 # Create your views here.
 
-class BookCreateView(BSModalCreateView):
-    template_name = 'library/books/createBook.html'
-    form_class = BookForm
-    success_message = 'Success: Book was created'
-    success_url = reverse_lazy('library:home')
 
+class BookCreateView(BSModalCreateView):
+    template_name = "library/books/createBook.html"
+    form_class = BookForm
+    success_message = "Success: Book was created"
+    success_url = reverse_lazy("library:home")
 
 
 def book_list(request, genre_slug=None):
     genre = None
-    genres = Genre.objects.all ( )
+    genres = Genre.objects.all()
     books = Book.objects.all()
 
     if genre_slug:
-        genre = get_object_or_404 (Genre, slug=genre_slug)
-        books = books.filter (genre=genre)
+        genre = get_object_or_404(Genre, slug=genre_slug)
+        books = books.filter(genre=genre)
 
-    return render (request,
-                   'library/books/list.html',
-                   {
-                       'genres': genres,
-                       'genre': genre,
-                       'books': books
-                   })
+    return render(
+        request,
+        "library/books/list.html",
+        {"genres": genres, "genre": genre, "books": books},
+    )
 
 
 def book_detail(request, id, slug):
-    book = get_object_or_404 (Book,
-                              id=id,
-                              slug=slug,
-                              available=True)
-    return render (request,
-                   'library/books/detail.html',
-                   {
-                       'book': book,
-
-                   })
+    book = get_object_or_404(Book, id=id, slug=slug, available=True)
+    return render(
+        request,
+        "library/books/detail.html",
+        {
+            "book": book,
+        },
+    )
 
 
 def author_detail(request, id, slug):
-    author = get_object_or_404 (Author,
-                                id=id,
-                                slug=slug)
-    return render (request,
-                   'library/author/authorDetail.html',
-                   {
-                       'author': author,
-                   })
+    author = get_object_or_404(Author, id=id, slug=slug)
+    return render(
+        request,
+        "library/author/authorDetail.html",
+        {
+            "author": author,
+        },
+    )
 
 
 def author_list(request):
-    authors = Author.objects.values('first_name', 'last_name').distinct()
+    authors = Author.objects.values("first_name", "last_name").distinct()
 
-
-    return render (request,
-                   'library/author/authorList.html',
-                   {
-                       'authors': authors
-                   })
+    return render(request, "library/author/authorList.html", {"authors": authors})
 
 
 def home(request):
 
     if request.user.is_authenticated:
-        return redirect(reverse ('accounts:customerView'))
+        return redirect(reverse("accounts:customerView"))
     else:
-        return render (request, 'home.html')
+        return render(request, "home.html")
+
+
+def contactus(request):
+    return render(request, "contactus.html")
